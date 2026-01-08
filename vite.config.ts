3dc6a700ffbd66ef6af24d92807dc9e1e61323f4
Barbara Ścibior <barbara.scibior@schibsted.com>
2026-01-08 14:47:09 +0100

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
@@ -10,11 +9,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: '/SisiWebStudio.github.io/',
-  resolve: {
-    alias: {
-      // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base: '/SisiWebStudio/',
 })
